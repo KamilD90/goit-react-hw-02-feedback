5b1b28e7936f875e07854f7ecfabf3a94c1be778
KamilD90 <117952413+KamilD90@users.noreply.github.com>
2023-06-16 01:26:34 +0200

    styles done

--- src/components/feedback/Feedback.jsx
+++ src/components/feedback/Feedback.jsx
@@ -48,19 +48,51 @@ import { Component } from "react";
         return (
             <div className={css.wrapper}>
                 <h2 className={css.title}>Please leave your feedback</h2>
-                <button type="button" onClick={this.handlePositive} className={[css.btn, css.btn__positive]}>Good</button>
-                <button type="button" onClick={this.handleNeutral} className={[css.btn, css.btn__neutral]}>Neutral</button>
-                <button type="button" className={[css.btn, css.btn__negative]} onClick={this.handleNegative}>Bad</button>
-                <h3 className='css.subtitle'>Statistic</h3>
-                <ul className='statList'>
-                  <li className='statList_item'><span className='statList__item-name'>Positive:<span className='statList__item-value'>{good}</span></span></li>
-                  <li className='statList_item'><span className='statList__item-name'>Neutral:<span className='statList__item-value'>{neutral}</span></span></li>
-                  <li className='statList_item'><span className='statList__item-name'>Negative:<span className='statList__item-value'>{bad}</span></span></li>
-                  <li className='statList_item'><span className='statList__item-name'>Total:<span className='statList__item-value'>{total}</span></span></li>
-                  {total !==0 && (<li className='statList_item'><span className='statList__item-name'>Positive Feedback:<span className='statList__item-value'>{positivePercentage.toFixed(1)}%</span></span></li>)}
-                   
+                <div className={css.btn__box}>
+                    <button type="button" onClick={this.handlePositive} className={[ css.btn, css.btn__positive ].join(' ')}>Positive</button>
+                    <button type="button" onClick={this.handleNeutral} className={[css.btn, css.btn__neutral].join(' ')}>Neutral</button>
+                    <button type="button" className={[css.btn, css.btn__negative].join(' ')} onClick={this.handleNegative}>Negative</button>
+                </div>
+             
+                <h3 className={css.subTitle}>Statistic</h3>
+                {total === 0 ? (
+                <h5 className={css.noFeedback}>No Feedback</h5>
+                ) : (
+                <ul className={css.statList}>
+                    <li className={css.statList_item}>
+                    <span className={css.statList__item__name}>
+                        Positive:
+                    </span>
+                    <span className={css.statList__item__value}>{good}</span>
+                    </li>
+                    <li className={css.statList_item}>
+                    <span className={css.statList__item__name}>
+                        Neutral:
+                    </span>
+                    <span className={css.statList__item__value}>{neutral}</span>
+                    </li>
+                    <li className={css.statList_item}>
+                    <span className={css.statList__item__name}>
+                        Negative:
+                    </span>
+                    <span className={css.statList__item__value}>{bad}</span>
+                    </li>
+                    <li className={css.statList_item}>
+                    <span className={css.statList__item__name}>
+                        Total:
+                    </span>
+                    <span className={css.statList__item__value}>{total}</span>
+                    </li>
+                    <li className={css.statList_item}>
+                    <span className={css.statList__item__name}>
+                        Positive Feedback:
+                    </span>
+                    <span className={css.statList__item__value}>{positivePercentage.toFixed(1)}%</span>
+                    </li>
                 </ul>
-            </div>
+                )}
+
+      </div>
         );
     }
 
